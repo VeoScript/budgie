541c11a705967352c696a0dcd8148a81cbd5262e
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2022-02-15 09:01:47 +0800

    feat: add images domain

--- next.config.js
+++ next.config.js
@@ -8,6 +8,11 @@ const nextConfig = {
     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
     FACEBOOK_CLIENT_ID: process.env.FACEBOOK_CLIENT_ID,
     FACEBOOK_CLIENT_SECRET: process.env.FACEBOOK_CLIENT_SECRET
+  },
+  images: {
+    domains: [
+      'lh3.googleusercontent.com'
+    ]
   }
 }
 
