58b7d1284c056c5761f8e736d3a563be63e4c85f
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2022-02-16 16:22:07 +0800

    chore: import nextjs progressbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import '../styles/tailwind.css'
 import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react'
+import NextJSProgress from '../lib/NextJSProgressbar'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
+      <NextJSProgress />
       <Component {...pageProps} />
     </SessionProvider>
   )
