62987b910235748e00e12b945e908b1412c5a370
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2022-02-28 16:58:10 +0800

    feat: add expenses-report page and update signin page layout (#16)

---
+++ pages/expenses-report.tsx
@@ -0,0 +1,118 @@
+import { NextPage, GetServerSideProps } from 'next'
+import React from 'react'
+import Head from 'next/head'
+import Router from 'next/router'
+import Layout from '../layouts/default'
+import DisplayExpensesReport from '../components/DisplayExpensesReport'
+import { useSession, getSession } from 'next-auth/react'
+import useSWR from 'swr'
+import prisma from '../lib/Prisma'
+import Loading from '../layouts/loading'
+
+const fetcher = async (
+  input: RequestInfo,
+  init: RequestInit,
+  ...args: any[]
+) => {
+  const res = await fetch(input, init)
+  return res.json()
+}
+
+interface TypeProps {
+  getUser: any
+  getBudget: any
+}
+
+const ExpensesReport: NextPage<TypeProps> = ({ getUser, getBudget }) => {
+
+  const { data: session, status } = useSession()
+
+  const { data: user } = useSWR(`/api/auth/user/${getUser.id}`, fetcher, {
+    refreshInterval: 1000,
+    fallbackData: getUser
+  })
+
+  const { data: budget } = useSWR(`/api/expenses-report/${getUser.id}`, fetcher, {
+    refreshInterval: 1000,
+    fallbackData: getBudget
+  })
+
+  React.useEffect(() => {
+    if (!session) {
+      Router.push('/signin')
+      return
+    }
+  })
+
+  if (status === "loading") {
+    return (
+      <Loading />
+    )
+  }
+
+  return (
+    <React.Fragment>
+      <Head>
+        <title>Expenses Report</title>
+      </Head>
+      <Layout
+        getUser={user}        
+      >
+        <DisplayExpensesReport getBudget={budget} />
+      </Layout>
+    </React.Fragment>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const session = await getSession(ctx)
+  
+  const getUser = await prisma.user.findFirst({
+    where: {
+      email: session?.user?.email
+    },
+    select: {
+      id: true,
+      image: true,
+      name: true,
+      email: true,
+      username: true,
+      location: true
+    }
+  })
+
+  const getBudget = await prisma.budget.findMany({
+    where: {
+      userId: getUser?.id
+    },
+    orderBy: [{
+      counter: 'desc'
+    }],
+    select: {
+      id: true,
+      counter: true,
+      name: true,
+      currency: true,
+      balance: true,
+      income: true,
+      expense: true,
+      budgetDetails: {
+        select: {
+          id: true,
+          name: true,
+          type: true
+        }
+      }
+    },
+  })
+
+  return {
+    props: {
+      session,
+      getUser,
+      getBudget
+    }
+  }
+}
+
+export default ExpensesReport
