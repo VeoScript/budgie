e72e48858833fae6b309fe948b7860035e3d2e87
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2022-08-10 09:22:48 +0800

    style: update max width for wide screens

--- layouts/default.tsx
+++ layouts/default.tsx
@@ -9,7 +9,7 @@ interface TypeProps {
 const Layout: React.FC<TypeProps> = ({ children, getUser }) => {
   return (
     <div className="font-titilliumweb flex justify-center w-full h-screen overflow-y-auto md:overflow-hidden">
-      <div className="flex flex-col w-full max-w-[2400px] h-full">
+      <div className="flex flex-col items-center w-full max-w-[1401px] h-full">
         <NavBar getUser={getUser} />
         { children }
       </div>
