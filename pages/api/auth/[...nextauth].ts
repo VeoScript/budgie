793972b1e09b446d9b04f69546028316591731dd
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2023-06-28 19:20:16 +0800

    chore: remove facebook third-party authentication

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,6 +1,5 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
-import FacebookProvider from "next-auth/providers/facebook"
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import { PrismaClient } from "@prisma/client"
 
@@ -13,10 +12,10 @@ export default NextAuth({
       clientId: `${process.env.GOOGLE_CLIENT_ID}`,
       clientSecret: `${process.env.GOOGLE_CLIENT_SECRET}`,
     }),
-    FacebookProvider({
-      clientId: `${process.env.FACEBOOK_CLIENT_ID}`,
-      clientSecret: `${process.env.FACEBOOK_CLIENT_SECRET}`,
-    }),
+    // FacebookProvider({
+    //   clientId: `${process.env.FACEBOOK_CLIENT_ID}`,
+    //   clientSecret: `${process.env.FACEBOOK_CLIENT_SECRET}`,
+    // }),
   ],
   session: {
     strategy: "database",
